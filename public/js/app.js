b67f3d160c1c784749344b0a6fb84cac00622476
Tamar Moshe <tamarmoshe14@gmail.com>
2021-01-22 10:23:49 +0200

    remove unnecesary console

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("client side js is loading.")
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
